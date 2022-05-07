f4e8ef883c653c75d8d7ac915fa944f5663aa6bc
pauliancu97 <paul97iancu@gmail.com>
2022-05-07 14:59:56 +0300

    initial commit

---
+++ rust-grep/target/rls/debug/deps/rust_grep-6ceef035d0c74cd5.d
@@ -0,0 +1,5 @@
+/home/pauli/rust-grep/rust-grep/rust-grep/target/rls/debug/deps/rust_grep-6ceef035d0c74cd5.rmeta: src/main.rs
+
+/home/pauli/rust-grep/rust-grep/rust-grep/target/rls/debug/deps/rust_grep-6ceef035d0c74cd5.d: src/main.rs
+
+src/main.rs:
---
+++ rust-grep/target/rls/debug/deps/rust_grep-85fc1da52b7ab6c8.d
@@ -0,0 +1,5 @@
+/home/pauli/rust-grep/rust-grep/rust-grep/target/rls/debug/deps/rust_grep-85fc1da52b7ab6c8.rmeta: src/main.rs
+
+/home/pauli/rust-grep/rust-grep/rust-grep/target/rls/debug/deps/rust_grep-85fc1da52b7ab6c8.d: src/main.rs
+
+src/main.rs:
